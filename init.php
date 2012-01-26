feec95a539dfa083b094b15a8f1519897608824c
Dave Widmer <dwidmer@bgsu.edu>
2012-01-26 09:30:55 -0500

    Fixing the vendor bootstrap (forgot vender folder...)

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Setup the autoloaders for Owl and Cactus
-include "cactus".DIRECTORY_SEPARATOR."bootstrap.php";
-include "owl".DIRECTORY_SEPARATOR."bootstrap.php";
+include "vendor".DIRECTORY_SEPARATOR."cactus".DIRECTORY_SEPARATOR."bootstrap.php";
+include "vendor".DIRECTORY_SEPARATOR."owl".DIRECTORY_SEPARATOR."bootstrap.php";
 
 // Load up the mobile detection file
-include "mobile".DIRECTORY_SEPARATOR."Detect.php";
+include "vendor".DIRECTORY_SEPARATOR."mobile".DIRECTORY_SEPARATOR."Detect.php";
